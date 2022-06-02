d49c3dac7c6fbb6271bc87c1b49d88fcca8c19ce
Lautaro Tummino <lautarotummino@gmail.com>
2022-06-02 20:32:26 -0300

    Corrijiendo teste del primer ejercicio

--- tests/test_ejercicio_1_LautaroTummino.py
+++ tests/test_ejercicio_1_LautaroTummino.py
@@ -3,7 +3,7 @@
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
-from src.ejercicio1.py import par_e_impar
+from ejercicio1.py import par_e_impar
 
 """
 Describir aquí que es lo que se esta probando.
@@ -14,21 +14,19 @@ que el archivo a probar agregando antes `test_`
 
 def test_par_e_impar_positivos():
     """
-    Esta función testeara ls funcion de pares e impares, ejercicio N°1
+    Esta función testeara ls funcion de pares e impares positivos, ejercicio N°1
     """
     numero = 10
     resultado = par_e_impar(numero)
-    assert isinstance(resultado, bool), "El resultado debe ser un valor booleano
-    assert resultado == True "True por que numero es primo"
+    assert isinstance(resultado, bool), "El resultado debe ser un valor booleano"
+    assert resultado == True, "True por que el numero es primo"
 
 
 def test_par_e_impar_negativos():
     """
-    Esta función testeara ls funcion de pares e impares, ejercicio N°1
+    Esta función testeara las funcion de pares e impares negativos, ejercicio N°1
     """
     numero = -15
     resultado = par_e_impar(numero)
-    assert isinstance(resultado, bool), "El resultado debe ser un valor booleano
-    assert resultado == False "False por que no lo es"
-
-    
+    assert isinstance(resultado, bool), "El resultado debe ser un valor booleano"
+    assert resultado == False, "False por que no lo es"
