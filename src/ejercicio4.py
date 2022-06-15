05a848c0ec84643f38d65e674b1014b2b0c32139
Lautaro Tummino <lautarotummino@gmail.com>
2022-06-15 14:38:46 -0300

    Se agrega una pseudo solución del ejercicio 5, y el n-esimo termino de fibonacci

--- src/ejercicio4.py
+++ src/ejercicio4.py
@@ -4,11 +4,35 @@
 ################
 
 """
-mo una tuple.
-Sin utilizar lazos for o las funciones integradas del lenguaje que procesan secuencias.
+4. Fibonacci
+La sucesión de Fibonacci es una sucesión infinita donde cada
+elemento es la suma de los dos anteriores.
+En la misma, los dos primeros términos son 1.
+(En algunos lugares se toma el primer término en 0 y el segundo en 1)
+Implementar una función que permita obtener el n-esimo termino de la
+sucesión de Fibonacci. Siendo este número un entero positivo mayor a 2.
 """
 
 
+def fibonacci(num):
+    """
+    Esta función se encarga de encontrar el n-esimo número
+    de una secuencia númerica, siempre y cuando el número sea mayor a 2
+    Precondicion: Ingresar un número mayor a 2
+    Postcondicion: Imprimir por pantalla el n-esimo termino.
+    """
+    if num < 2:
+        print("Error, Intente con un numero mayor a 2")
+    else:
+        ant = num - 1
+        total = ant + num
+        result = []
+        while total < 1550000:
+            total = ant + num
+            ant = num
+            num = total
+            result = result + [num]
+    return result[8]
 
 
 def principal():
@@ -16,6 +40,10 @@ def principal():
     Esta función es la que se encarga de la parte 'interactiva' del ejercicio
     (La entrada, la llamada al algoritmo y la salida)
     """
+    num = int(input("Ingrese un número N mayor que 2: "))
+    fibo = fibonacci(num)
+    print(f"El n-esimo termino es: {fibo}")
+
 
 if __name__ == "__main__":
     principal()
--- src/ejercicio5.py
+++ src/ejercicio5.py
@@ -4,11 +4,46 @@
 ################
 
 """
-mo una tuple.
-Sin utilizar lazos for o las funciones integradas del lenguaje que procesan secuencias.
+5. Corchetes balanceados
+Implementar una función que determine si
+una cadena con corchetes está balanceada.
+Es decir, si cada corchete que abre, tiene su par que cierra.
+El resultado debe ser un valor lógico indicando si esta o no balanceado.
 """
 
 
+def corchetes_balanceados(cadena):
+    """
+    Esta función se encarga de corroborar si todos los: '(' '{' '[' que se abren
+    en una cadena se cierran ')' ']' '}'. Retornando True si sucede, False si no.
+    Aun no logro que ignore todos los caracteres, si le pasamos una cadena sin
+    corchetes, el resultado va a ser TRUE cuando debria ser FALSE
+    Precondicion: Ingresar una cadena de caracteres con llaves, parentesis
+    o corchetes ej: (hola), ]hola(.
+    Postcondicion: Retornar un valor booleano, True si cierran False si no.
+    """
+    contador = 0
+    sumador = 0
+    cont = 0
+    for elemento in cadena:
+        if elemento == "(":
+            contador += 1
+        if elemento == ")":
+            contador -= 1
+        if elemento == "[":
+            sumador += 1
+        if elemento == "]":
+            sumador -= 1
+        if elemento == "{":
+            cont += 1
+        if elemento == "}":
+            cont -= 1
+        if contador < 0 or sumador < 0 or cont < 0:
+            return False
+    if contador == 0 and sumador == 0 and cont == 0:
+        return True
+    else:
+        return False
 
 
 def principal():
@@ -16,6 +51,10 @@ def principal():
     Esta función es la que se encarga de la parte 'interactiva' del ejercicio
     (La entrada, la llamada al algoritmo y la salida)
     """
+    cadena = input("Ingrese untexto con (, ] o {")
+    corroborando = corchetes_balanceados(cadena)
+    print(corroborando)
+
 
 if __name__ == "__main__":
     principal()
